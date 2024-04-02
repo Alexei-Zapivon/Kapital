6bb3509cac5664cee06067082d318b4576e29c5d
Blizzard <alexsjG7@gmail.com>
2024-04-02 17:50:20 +0300

    job

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,9 @@
 var progressBar = document.getElementById('progress');
 var balans = document.getElementById('balans'); 
-var blocks = document.querySelectorAll('.block');
-var currentBlockIndex = 0;
-var amounta = 1000;
-
-balans.innerHTML = amounta;
+var price = document.getElementById('price'); 
 
 // Функция для увеличения прогресса
-function increaseProgress() {
+function increaseProgress(progressBar) {
     var width = 0;
     var interval = setInterval(function() {
         width += 1;
@@ -15,18 +11,34 @@ function increaseProgress() {
         if (width >= 100) {
             clearInterval(interval);
             width = 0;
-            amounta += 100;
+            amounta += 1000;
             balans.innerHTML = amounta;
-            setTimeout(increaseProgress);
+            setTimeout(increaseProgress, 15, progressBar);
         }
     }, 15);
 }
+var amounta = 1000;
+var currentBlockIndex = 0;
+var jobPrice = [1000, 10000, 20000];
 
-increaseProgress();
+balans.innerHTML = amounta;
 
 function openNextBlock() {
+    var progressBarId = 'progress' + (currentBlockIndex + 1);
+    var progressBar = document.getElementById(progressBarId);
+    var blocks = document.querySelectorAll('.block');
     if (currentBlockIndex < blocks.length) {
-        blocks[currentBlockIndex].style.display = "inline-flex";
-        currentBlockIndex++;
+        if (amounta >= jobPrice[currentBlockIndex]) {
+            amounta -= 1000;
+            balans.innerHTML = amounta;
+            blocks[currentBlockIndex].style.display = "inline-flex";
+            increaseProgress(progressBar);
+            currentBlockIndex++;
+            price.innerHTML = jobPrice[currentBlockIndex]; // Установка цены после увеличения индекса
+        } else {
+            alert("У вас не достаточно средств");
+        }
     }
 }
+
+price.innerHTML = jobPrice[currentBlockIndex];
