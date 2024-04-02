11090caa3e590df7e09915b3c950c4563265c86a
Blizzard <alexsjG7@gmail.com>
2024-04-02 17:05:01 +0300

    blocks

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,10 @@
 var progressBar = document.getElementById('progress');
+var balans = document.getElementById('balans'); 
+var blocks = document.querySelectorAll('.block');
+var currentBlockIndex = 0;
+var amounta = 1000;
+
+balans.innerHTML = amounta;
 
 // Функция для увеличения прогресса
 function increaseProgress() {
@@ -9,9 +15,18 @@ function increaseProgress() {
         if (width >= 100) {
             clearInterval(interval);
             width = 0;
+            amounta += 100;
+            balans.innerHTML = amounta;
             setTimeout(increaseProgress);
         }
     }, 15);
 }
+
 increaseProgress();
 
+function openNextBlock() {
+    if (currentBlockIndex < blocks.length) {
+        blocks[currentBlockIndex].style.display = "inline-flex";
+        currentBlockIndex++;
+    }
+}
