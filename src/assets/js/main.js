91294afc63ef56edc3052206d46c8d2eca65ca8c
Blizzard <alexsjG7@gmail.com>
2024-04-03 18:22:17 +0300

    image

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -33,19 +33,44 @@ function increaseProgress(progressBar, money) {
     }, 30);
 }
 
+var imageNames = [
+    'image1.jpg', 'image2.jpg', 'image3.jpg',
+    'image4.jpg', 'image5.jpg', 'image6.jpg',
+    'image7.jpg', 'image8.jpg', 'image9.jpg',
+    'image10.jpg'
+];
+
+var descriptions = [
+    "Сдавать бутылки (Это хотя бы что-то)",
+    "Сдавать металл (Как у нас в городе не хватает алюминия!)",
+    "Быть вором (Лучше не берите с собой мешок)",
+    "Стать дворником (Тут проблема с отпуском, но мусор тоже любит компанию)",
+    "Пойти на стройку (Только не забудьте шлем)",
+    "Устроиться в продуктовый (Сладости бесплатно? Да, пожалуйста!)",
+    "Стать парикмахером (Волосы клиентов - ваше полотно искусства!)",
+    "Работать стилистом (Волосы клиентов - ваша холст)",
+    "Варить компот (Когда жизнь дает вам фрукты...)",
+    "Открыть бизнес (Теперь вы ваш собственный начальник! Поздравляем!)"
+];
+
+
 function createBlock(blockNumber) {
+    var imageName = imageNames[blockNumber % imageNames.length];
+    var imagePath = `/src/assets/img/${imageName}`;
+    
     var block = `
     <div class="block block${blockNumber}">
-        <img class="image-job" src="/src/assets/img/Buling.jpg">
+        <img class="image-job" src="${imagePath}">
         <div class="info">
             <div class="progress-bar" id="progress${blockNumber}"></div>
-            <div class="title-info">Работа на стройке</div>
+            <div class="title-info">${descriptions[blockNumber]}</div> <!-- Используем описание из массива descriptions -->
             <div class="profit-title">Прибыль в день: <div class="">${profits}</div></div>
             <button class="button-clicker" onclick="cliker()">Надорвать спину</button>
         </div>
     </div>`;
     return block;
 }
+
 
 function cliker() {
     profitBlock(klikerBuy);
