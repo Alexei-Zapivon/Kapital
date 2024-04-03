b0e968942a15440e81f82216cc981db902d3a3a4
Blizzard <alexsjG7@gmail.com>
2024-04-03 17:52:03 +0300

    kliker

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,54 +1,74 @@
-//var progressBar = document.getElementById('progress');
-//var balansId = document.getElementById('balans'); 
-//var price = document.getElementById('price'); 
-//var amounta = 300;
-//
-//balans.innerHTML = amounta;
-//
-////function increaseProgress(progressBar) {
-////    var width = 0;
-////    var interval = setInterval(function() {
-////        width += 1;
-////        progressBar.style.width = width + '%';
-////        if (width >= 100) {
-////            clearInterval(interval);
-////            width = 0;
-////            amounta += profit;
-////            balans.innerHTML = amounta;
-////            setTimeout(increaseProgress, 15, progressBar);
-////        }
-////    }, 15);
-////}
-//
-//var jobPrice = [300, 1000, 12000, 16000, 20000];
-//var gain = [0, 100, 300, 500, 700, 1300];
-//var currentBlockIndex = 0;
-//
-//function openNextBlock() {
-//    var progressBarId = 'progress' + (currentBlockIndex + 1);
-//    var progressBar = document.getElementById(progressBarId);
-//    var blocks = document.querySelectorAll('.block');
-//
-//    if (currentBlockIndex < blocks.length) {
-//        if (amounta >= jobPrice[currentBlockIndex]) {
-//            amounta -= jobPrice[currentBlockIndex];
-//            balans.innerHTML = amounta;
-//            blocks[currentBlockIndex].style.display = "inline-flex";
-//            increaseProgress(progressBar);
-//
-//            var profit = blocks[currentBlockIndex].querySelector('.profit'); 
-//            currentBlockIndex++;
-//            profit.innerHTML = gain[currentBlockIndex]; 
-//            price.innerHTML = jobPrice[currentBlockIndex];
-//        } else {
-//            alert("У вас не достаточно средств");
-//        }
-//    }
-//}
-//
-//
-//console.log(profit)
-//price.innerHTML = jobPrice[currentBlockIndex];
-//var profit = document.querySelector('.profit'); 
-//profit.innerHTML = gain[currentBlockIndex];
-//
+var main = document.getElementById('main');
+var balansId = document.getElementById('balans'); 
+var price = document.getElementById('price'); 
+
+var blockIndex = 0;
+var balans = 300;
+var klikerBuy = 10;
+
+var profits = [100, 300, 400, 500, 600, 700, 800];
+var priceNewBlock = [300, 1000, 2000, 6000, 8000, 12000, 20000]
+
+balansId.innerHTML = balans;
+price.innerHTML = priceNewBlock[blockIndex];
+
+function profitBlock(money) {
+    balans += money
+}
+
+function increaseProgress(progressBar, money) {
+    var width = 0;
+    var interval = setInterval(function() {
+        width += 1;
+        progressBar.style.width = width + '%';
+        if (width >= 100) {
+            clearInterval(interval);
+            width = 0;
+            profitBlock(money); 
+            balansId.innerHTML = balans;
+            setTimeout(function() {
+                increaseProgress(progressBar, money);
+            }, 30);
+        }
+    }, 30);
+}
+
+function createBlock(blockNumber) {
+    var block = `
+    <div class="block ${blockNumber}">
+        <img class="image-job" src="/src/assets/img/Buling.jpg">
+        <div class="info">
+            <div class="progress-bar" id="progress${blockNumber}"></div>
+            <div class="title-info">Работа на стройке</div>
+            <div class="profit-title">Прибыль в день: <div class="">${profits[blockNumber]}</div></div>
+            <button class="button-clicker" onclick="cliker()">Надорвать спину</button>
+        </div>
+    </div>`;
+    return block;
+}
+
+function cliker() {
+    profitBlock(klikerBuy);
+    balansId.innerHTML = balans;
+}
+
+function bafLife() {
+    balans -= 100;
+    klikerBuy += 10
+    balansId.innerHTML = balans;
+}
+
+function openNextBlock() {
+    if (balans >= 0 && balans - priceNewBlock[blockIndex] >= 0 && blockIndex < profits.length) { 
+        var money = profits[blockIndex];
+        main.insertAdjacentHTML('beforeend', createBlock(blockIndex));
+        var progressBar = document.getElementById('progress' + blockIndex);
+        increaseProgress(progressBar, money); 
+        balans -= priceNewBlock[blockIndex];
+        balansId.innerHTML = balans;
+        blockIndex++; 
+        price.innerHTML = priceNewBlock[blockIndex];
+    }
+}
+
+
